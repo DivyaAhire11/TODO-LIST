7c18f571ac23d98a434f30125674d2d7c83e8946
DivyaAhire11 <divyaahire121@gmail.com>
2025-11-02 18:59:02 +0530

    Add delete and complete toggle

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
+
+
 function addTask() {
-  let taskInput = document.getElementById("taskInput");
-  let taskText = taskInput.value.trim();
-  if (taskText === "") {
-    alert("Please enter a task!");
-    return;
-  }
-  let li = document.createElement("li");
-  li.textContent = taskText;
-  document.getElementById("taskList").appendChild(li);
-  taskInput.value = "";
+    let taskInput = document.getElementById("taskInput");
+    let taskText = taskInput.value.trim();
+    if (taskText === "") {
+        alert("Please enter a task!");
+        return;
+    }
+    let li = document.createElement("li");
+    li.textContent = taskText;
+    document.getElementById("taskList").appendChild(li);
+    taskInput.value = "";
 }
+
+
+li.addEventListener("click", () => {
+    li.classList.toggle("completed");
+});
+
+let delBtn = document.createElement("button");
+delBtn.textContent = "Delete";
+delBtn.classList.add("delete-btn");
+delBtn.onclick = function () {
+    li.remove();
+};
+li.appendChild(delBtn);
